f636b71c2241d8b3569b37a507f171e249d468bc
LeeRoa <132882290+LeeRoa@users.noreply.github.com>
2025-09-01 22:50:21 +0900

    프로젝트 init

---
+++ src/main/java/com/roa/forge/RoaForgeApplication.java
@@ -0,0 +1,13 @@
+package com.roa.forge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoaForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoaForgeApplication.class, args);
+    }
+
+}

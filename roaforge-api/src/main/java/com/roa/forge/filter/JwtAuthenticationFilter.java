5c14ec61afdd0b8c772cf848cf67c70068fd4466
roa7717 <roa7717@kcert.co.kr>
2025-09-18 17:50:05 +0900

    불필요한 임포트 제거

--- roaforge-api/src/main/java/com/roa/forge/filter/JwtAuthenticationFilter.java
+++ roaforge-api/src/main/java/com/roa/forge/filter/JwtAuthenticationFilter.java
@@ -4,23 +4,18 @@ import com.roa.forge.dto.UserPrincipal;
 import com.roa.forge.entity.UserAccount;
 import com.roa.forge.provider.JwtTokenProvider;
 import com.roa.forge.repository.UserAccountRepository;
-import com.roa.forge.service.CustomUserDetailsService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 
 @Component
